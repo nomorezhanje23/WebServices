29dba94b9129740939be653ce047d935c06da37c
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-10-07 23:20:10 +0200

    Lesson 1 video

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Nomore Zhanje!");
+});
+
+ //this avoids code duplication
+const port = 3000;
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});
