a2b560c36d9dd6d86a954eee1da0f5219d90d0b6
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-10-08 00:01:53 +0200

    web creation

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Nomore Zhanje!");
+  res.send("Nomore Tawanda!");
 });
 
- //this avoids code duplication
-const port = 3000;
-
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
