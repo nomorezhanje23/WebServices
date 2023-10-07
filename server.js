42ec02725d3e20526767be368baca4faac92e7bf
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-10-08 01:30:30 +0200

    Contacts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Nomore Tawanda!");
+  res.send("Contacts by Nomore Tawanda!");
 });
 
 app.listen(process.env.PORT || 3000, () => {
